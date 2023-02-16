c5df953482d49ad916eb70d3fd09a317263fd329
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2023-02-16 13:33:21 -0800

    correction 2

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,8 +23,6 @@ def page_not_found(error):
     """diplays error 404 if page not found"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-
 if __name__ == "__main__":
-
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', '5000')))
